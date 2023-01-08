a8f33c871ac77b69dabeaed4f808587ee1c238bb
Pratham Agrawal <prathamagrawal1205@gmail.com>
2023-01-09 02:01:40 +0530

    Added edge detection

---
+++ ml/rename.py
@@ -0,0 +1,14 @@
+
+import os
+def main():
+   
+    folder = "./data/"
+    for count, filename in enumerate(os.listdir(folder)):
+        dst = f"Image{str(count)}.tif"
+        src =f"{folder}/{filename}"  
+        dst =f"{folder}/{dst}"
+        os.rename(src, dst)
+ 
+# Driver Code
+if __name__ == '__main__':
+    main()
